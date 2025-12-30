9975891428b36b7e5325f978394e9304e53cdc3b
Prashant Dubey <pdubey1924@gmail.com>
2025-12-30 21:53:57 +0530

    fate data removed

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
-
 import (
 	"cliqk-copilot/handlers"
 	"github.com/gin-gonic/gin"
 )
-
 func main() {
 	r := gin.Default()
-
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "Marketing Copilot API running 🚀",
 		})
 	})
-
 	r.POST("/generate/ad-copy", handlers.GenerateAdCopy)
-
 	r.Run(":8080")
 }
