a7d0603d76f4bdb8bd82a33283e502e151709035
Prashant Dubey <pdubey1924@gmail.com>
2026-01-02 11:56:54 +0530

    UP

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
+
 import (
 	"cliqk-copilot/handlers"
 	"github.com/gin-gonic/gin"
 )
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"status": "Marketing Copilot API running 🚀",
+			"status": "Marketing Copilot API running ",
 		})
 	})
-	r.POST("/generate/ad-copy", handlers.GenerateAdCopy)
+	
 	r.Run(":8080")
 }
